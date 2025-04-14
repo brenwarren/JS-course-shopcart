c88f414387e2ce2c6675ff85a1b176b45c0fdb4c
brenwarren <brenwarren@me.com>
2025-04-14 15:43:47 +0200

    Removed testing modules and function

--- src/assets/script.js
+++ src/assets/script.js
@@ -131,25 +131,6 @@ function pay(amount) {
   return remainingBalance;
 }
 
-/* Place stand out suggestions here (stand out suggestions can be found at the bottom of the project rubric.)*/
 
 
-/* The following is for running unit tests. 
-   To fully complete this project, it is expected that all tests pass.
-   Run the following command in terminal to run tests
-   npm run test
-*/
 
-module.exports = {
-  products,
-  cart,
-  addProductToCart,
-  increaseQuantity,
-  decreaseQuantity,
-  removeProductFromCart,
-  cartTotal,
-  pay,
-  emptyCart,
-  findProductInCart,
-  remainingBalance,
-};
